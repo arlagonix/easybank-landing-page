dba770dd531268792cacb5eaeaf37a19aaa80359
Arlagonix <gorbunov-sasha@yandex.ru>
2023-02-15 10:18:10 +0300

    Fixed error with interpreting index.styled.tsx as a separate page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  // https://stackoverflow.com/a/65598867/19449790
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 };
 
 module.exports = nextConfig;
